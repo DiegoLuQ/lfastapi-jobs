e37178f6a8ab594293f725b5d851052f72ca6286
DiegoLuQ <jdluquelinares@gmail.com>
2022-04-01 23:21:29 -0300

    Connect to database 2

---
+++ core/db/base_class.py
@@ -0,0 +1,24 @@
+from typing import Any
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
+
+"""
+declared_attrnormalmente se aplica como decorador a un método de nivel de clase, 
+convirtiendo el atributo en una propiedad similar a un escalar que se puede invocar desde la clase sin instanciar. 
+El proceso de mapeo declarativo busca estos declared_attrinvocables a medida que explora las clases y asume que cualquier 
+atributo marcado con declared_attrserá un invocable que producirá un objeto específico para el mapeo declarativo o la configuración de la tabla.
+
+declared_attrsuele aplicarse a mixins, para definir relaciones que se aplicarán a diferentes implementadores de la clase. 
+También se usa para definir Columnobjetos que incluyen la ForeignKeyconstrucción, ya que estos no se pueden reutilizar 
+fácilmente en diferentes asignaciones. El siguiente ejemplo ilustra ambos:
+"""
+"""
+Esta función hace uso del registry.as_declarative_base() método, primero creando registryautomáticamente y luego invocando al decorador.
+"""
+@as_declarative()
+class Base:
+    id : Any
+    __name__: str
+    def __tablename__(cls)->str:
+        return cls.__name__.lower()
+
---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from core.config import settings
+from core.db.session import engine
+from core.db.base_class import Base
+
+
+#https://docs.sqlalchemy.org/en/14/core/metadata.html?highlight=create_all#sqlalchemy.schema.MetaData.create_all
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+    
+#inicia la aplicacion
+def start_application():
+    app = FastAPI(title = settings.PROJECT_TITLE, version = settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+app = start_application()
+
+
+@app.get('/')
+def hello_api():
+    return {'deatil':'HelloWorld!'}
